f9c7cd2a110be73b6782c8fad9c7a3b93b4828ff
juanmacobo <juanma_cj_10@hotmail.com>
2016-02-28 20:45:08 +0100

    Ejercicio 1 json terminado

--- ej1json.py
+++ ej1json.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 
+#1- Lista el nombre de todas las calles de Zaragoza.
+
 import json
+
+with open("ejerciciojson.json") as archivo:
+	doc=json.load(archivo)
+
+for calles in doc["result"]:
+	print ""
+	print "NOMBRE calle: ",calles["calle"]["title"]
