4067336204517774aada49f3b8426a323814bff3
juanmacobo <juanma_cj_10@hotmail.com>
2016-02-28 21:32:04 +0100

    Ejercicio 3 json iniciado

--- ej3json.py
+++ ej3json.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 
+#3- Pedir por teclado el codigo de la calle y mostrar el nombre de las calles que tienen ese código.
+
 import json
+
+with open("ejerciciojson.json") as archivo:
+	doc=json.load(archivo)
+
+codigo=raw_input("Dime el código de la calle: ")
+
+for idcalle in doc["result"]:
+	if idcalle["calle"]["id"] == codigo:
+		print idcalle["calle"]["title"]
+	
