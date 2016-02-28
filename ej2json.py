e8fb093ea571000efe34f6052e2547965397d184
juanmacobo <juanma_cj_10@hotmail.com>
2016-02-28 21:06:14 +0100

    Ejercicio 2 json iniciado

--- ej2json.py
+++ ej2json.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
 
+#2- Contar el numero de calles que hay en Zaragoza.
+
 import json
 
+with open("ejerciciojson.json") as archivo:
+	doc=json.load(archivo)
+
+for calles in doc:
+	print len(calles)	
+	
