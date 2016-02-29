4add0858e6adfcb03d4fe1c882661cec8262da5f
juanmacobo <juanma_cj_10@hotmail.com>
2016-02-29 21:51:02 +0100

    Ejercicio 5 json iniciado

--- ej5json.py
+++ ej5json.py
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*-
 
+#5- Programa que cuente el número de barrios que hay en la ciudad y mostrar el nombre de cada uno de ellos.
+
 import json
+
+with open("ejerciciojson.json") as archivo:
+	doc=json.load(archivo)
+
+cont=0
+for barrios in doc["result"]:
+	if barrios["calle"]["tipo"]["name"] == barrio:
+		cont=cont+1
+print cont		
