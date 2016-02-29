e861fe1795679e1776b5f12af3070a04baffa186
juanmacobo <juanma_cj_10@hotmail.com>
2016-02-29 21:37:08 +0100

    Ejercicio 4 json iniciado

--- ej4json.py
+++ ej4json.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 
+#4- Pedir por teclado el codigo postal y mostrar el nombre y las coordenadas dondes se encuentra la calle.
+
 import json
+
+with open("ejerciciojson.json") as archivo:
+	doc=json.load(archivo)
+
+codigopostal=raw_input("Dime el codigo postal: ")
+
+for cod in doc["result"]:
+	if cod["codPos"] == codigopostal:
+		print cod["calle"]["title"]
+		
