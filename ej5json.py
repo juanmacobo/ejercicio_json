f3d5ddbd447b36ba29c0d176321ed59f7740a5a2
juanmacobo <juanma_cj_10@hotmail.com>
2016-03-05 21:06:40 +0100

    Ejercicio 5 json avanzado

--- ej5json.py
+++ ej5json.py
@@ -7,8 +7,34 @@ import json
 with open("ejerciciojson.json") as archivo:
 	doc=json.load(archivo)
 
+
+b=doc["result"]
 cont=0
-for barrios in doc["result"]:
-	if barrios["calle"]["tipo"]["name"] == "Barrio":
-		cont=cont+1
+for ba in b:
+	count=0
+	for s in ba:
+		if s == "barrio":
+			count=count+1
+	if count>0:
+		cont=cont+1	
 print cont		
+
+
+
+#b=doc["result"]
+
+#for ba in b:
+#	count=0
+#	for s in ba:
+#		if s == "barrio":
+#			count=count+1
+#	print count		
+#if = 1 ... +1
+
+
+
+#cont=0
+#for barrios in doc["result"]:
+#	if barrios["calle"]["tipo"]["name"] == "Barrio":
+#		cont=cont+1
+#print cont		
